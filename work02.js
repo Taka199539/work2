d2de20b863d263bdd348f030f5b75eea4bcf7323
Takayuki <taka19950309@gmail.com>
2021-02-14 07:22:37 +0000

    課題2

---
+++ work02.js
@@ -0,0 +1,20 @@
+(function($, window) {
+    $(function() {
+        //課題1. <h2>を非表示ではなく削除する。
+        $('h2').remove();
+        //課題2. #indexにtext()メソッドとhtmlメソッド(パラメータ無し)を実行し、違いを比較。
+        $('#index').html();
+        //課題3. ul#indexの最後に<li>メソッド</li>を追加
+        $('ul').append('<li>メソッド</li>');
+        //課題4. ul#indexの子要素<li>それぞれ文字列の長さをalertする
+        $('#index').find('li').each(function() {
+            alert($(this).text().length + '文字');
+        });
+        //課題5. 4でalertではなく、各文字列の最後にその長さを追加
+        $('#index').find('li').each(function() {
+            var count = ($(this).text().length);
+            $(this).append(count);
+        });
+    });
+})(jQuery, window);
+
