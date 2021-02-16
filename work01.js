0e354b9264f85c7558a7072dcef1045dc5ed81fe
Takayuki <taka19950309@gmail.com>
2021-02-16 09:05:50 +0000

    課題1修正

--- work01.js
+++ work01.js
@@ -2,25 +2,29 @@
 
 var answer = prompt('数字を入力してください');
 
-if (answer >= 0) {
+if (answer > 0) {
     alert('正の数');
-} else if(answer <= 0) {
+} else if(answer < 0) {
     alert('負の数');
-} 
+} else if(answer == 0) {
+    alert('0が入力されています');
+}
 
-//課題２. 1.で数値以外が入力された場合には「数値を入力してください」とalertする処理
+// //課題２. 1.で数値以外が入力された場合には「数値を入力してください」とalertする処理
 
 var answer = prompt('数字を入力してください');
 
-if (answer >= 0) {
+if (answer > 0) {
     alert('正の数');
-} else if(answer <= 0) {
+} else if(answer < 0) {
     alert('負の数');
+} else if(answer == 0) {
+    alert('0が入力されています');
 } else {
     alert('数値を入力してください');
 }
 
-//課題3.  配列に'パンダ','ライオン', 'キリン'を定義し、while文で全て表示する処理
+// //課題3.  配列に'パンダ','ライオン', 'キリン'を定義し、while文で全て表示する処理
 
 var animals = ['パンダ', 'ライオン', 'キリン'];
 var i = 0;
@@ -33,24 +37,26 @@ while (true) {
     i++;
 }
 
-//課題4. 3.でwhile文より前にpromptでキー入力し、その入力した内容が'ライオン'の場合に処理を中断
+// //課題4. 3.でwhile文より前にpromptでキー入力し、その入力した内容が'ライオン'の場合に処理を中断
 
 
 var animals = ['パンダ', 'ライオン', 'キリン'];
+
 var i = 0;
 
 var answer = prompt('パンダ、ライオン、キリンの中から選択する');
 
-while (i < animals.length) {
-    if (answer == 'ライオン') {
-        alert('ライオン');
+while (i <= animals.length) {
+    if(answer == animals[1]) {
         break;
     }
+    alert(animals[i]);
+    i++;
 }
 
 
 
-//課題5. 4で中断した際、’ライオン'の添字も'alert'するように変更。
+// //課題5. 4で中断した際、’ライオン'の添字も'alert'するように変更。
 
 var animals = ['パンダ', 'ライオン', 'キリン'];
 
@@ -58,16 +64,16 @@ var i = 0;
 
 var answer = prompt('パンダ、ライオン、キリンの中から選択する');
 
-while (i < animals.length) {
+var index = animals.indexOf(answer);
 
-    i++;
-
-    if (answer == 'ライオン') {
-        alert(answer + '[' + i + ']');
+for(var i = 0; i < animals.length; i++) {
+    
+    if(answer == 'ライオン') {
+        alert(answer + '[' + index + ']');
         break;
     }
-
     alert(animals[i]);
+    i++;
 }
 
 //課題6. オブジェクト変数の配列の形式で、動物と体重を表現する
@@ -86,7 +92,7 @@ alert('パンダは' + animals[0].weight + 'kgです');
 alert('ライオンは' + animals[1].weight + 'kgです');
 alert('キリンは' + animals[2].weight + 'kgです');
 
-//課題7. 6.でpromptで入力した動物名の体重をalertするように変更
+// //課題7. 6.でpromptで入力した動物名の体重をalertするように変更
 
 var animals = [
     {'name' : 'パンダ', 'weight' : 150},
@@ -97,23 +103,21 @@ var animals = [
 
 ];
 
-var  animal_weight
+var  animal_weight;
 var animal_name = prompt('パンダ、　ライオン、キリンを入力');
 
 for (i=0; i < animals.length; i++) {
     if (animals[i].name == animal_name) {
         animal_weight = animals[i].weight
+        alert(animal_weight　+ 'kgです');
     }
 }
 
-alert(animal_weight + 'kgです');
 
-
-
-//課題8. 縁の半径を入力するとその面積を返す関数を作り、その結果をalertする。
+// //課題8. 縁の半径を入力するとその面積を返す関数を作り、その結果をalertする。
 
 function calc(x)  {
-return x * x * 3.14;
+    return x * x * 3.14;
 }
 var answer = prompt('半径を入力する');
     alert (calc(answer));
